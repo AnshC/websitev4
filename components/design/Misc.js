10736cb74fe954224c53d96278e9d43d9f621b2a
Ansh Chauhan <anshc2005@gmail.com>
2025-09-29 18:19:07 -0400

    created component

---
+++ components/design/Misc.js
@@ -0,0 +1,14 @@
+export default function MiscSection() {
+  return (
+    <div>
+      <div className="absolute bottom-0 py-10 w-full px-40 z-20">
+        <h2 className="text-6xl tracking-tighter mb-[25px]">Misc.</h2>
+        <p className="text-lg w-[50%]">
+          A mix of random designs I’ve made along the way. Nothing too
+          serious—just experiments, side projects, and ideas I had fun bringing
+          to life.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ components/design/Projects.js
@@ -0,0 +1,15 @@
+export default function ProjectSection() {
+  return (
+    <div>
+      <div className="absolute bottom-0 py-10 w-full px-40 z-20">
+        <h2 className="text-6xl tracking-tighter mb-[25px]">Projects</h2>
+        <p className="text-lg w-[50%]">
+          A curated selection of collaborations with clients, where I translated
+          ideas into thoughtful, minimal designs. These projects highlight my
+          ability to balance creative vision with practical goals, delivering
+          work that is both functional and visually clear.
+        </p>
+      </div>
+    </div>
+  );
+}
