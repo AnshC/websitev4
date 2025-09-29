3a887ee213d7c326b34c006dd99f34440b016681
Ansh Chauhan <unsh@Anshs-MacBook-Pro-8.local>
2025-09-29 02:53:31 -0400

    updated shadow

--- components/text/ShadowText.js
+++ components/text/ShadowText.js
@@ -1,12 +1,13 @@
 export default function ShadowText({ children, className, alt }) {
   return (
-    <div className="relative">
+    <div className={`relative tracking-tighter font-bold text-9xl mb-[25px] w-max h-max ${className}`}>
+      <h2 className="invisible">{children}</h2> 
       <h2
-        className={`tracking-tighter font-bold text-9xl mb-[25px] px-40 ${className}`}
+        className={`absolute top-0`}
       >
         {children}
       </h2>
-      <h2 className={`tracking-tighter font-bold text-9xl mb-[25px] px-40 absolute left-3 bottom-3 ${alt ? "stroke-text-alt": "stroke-text"} text-foreground`}>
+      <h2 className={`absolute top-0 translate-x-3 -translate-y-3 ${alt ? "stroke-text-alt": "stroke-text"} text-foreground`}>
         {children}
       </h2>
     </div>
