e082eb39b28a0453de5a41f7e7070b3619de6972
Ansh Chauhan <anshc2005@gmail.com>
2025-09-30 03:14:16 -0400

    added ModelViewer

--- components/design/Highlights.js
+++ components/design/Highlights.js
@@ -1,6 +1,25 @@
+import ModelViewer from "@/components/external/ModelViewer";
+
 export default function HighlightSection() {
+
   return (
-    <div>
+    <div className="z-40 w-full h-full">
+      <div className="w-full h-full flex items-center justify-center">
+        <ModelViewer
+          url="/models/scene.glb"
+          width="100%"
+          height="100%"
+          defaultRotationX={-12} // <-- keep upright
+          defaultRotationY={80} // <-- no sideways tilt
+          enableManualRotation={false}
+          enableManualZoom={false}
+          modelYOffset={-2.05}
+          modelXOffset={1}
+          environmentPreset="sunset"
+          fadeIn={true}
+          showScreenshotButton={false}
+        />
+      </div>
       <div className="absolute bottom-0 py-10 w-full px-40 z-20">
         <h2 className="text-6xl tracking-tighter mb-[25px]">Highlights</h2>
         <p className="text-lg w-[50%]">
