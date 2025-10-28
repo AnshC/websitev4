3de964d1a9095b81f50e2786c0bc019f24c7af7d
Ansh Chauhan <anshc2005@gmail.com>
2025-10-28 04:44:35 -0400

    created about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,21 @@
-export default function About () {
-    return (
-        <div className="bg-background">
-            
-        </div>
-    )
+import Coffee from "@/components/about/Coffee";
+import Hero from "@/components/about/Hero";
+import Tools from "@/components/about/Tools";
+
+import styles from "./page.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.aboutComponent}>
+      <main className="h-[100vh]">
+        <Hero />
+      </main>
+      <section className="h-[100vh]">
+        <Coffee />
+      </section>
+      <section className="h-[100vh]">
+        <Tools />
+      </section>
+    </div>
+  );
 }
