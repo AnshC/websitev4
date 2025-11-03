f2bc251bafee8aa79a7a4372bd3f69a13f217775
Ansh Chauhan <anshc2005@gmail.com>
2025-11-03 16:19:24 -0500

    redesigned navigation

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,14 +1,16 @@
+import Button from "@/components/ui/Button";
 import DomeGallery from "@/components/ui/DomeGallery";
 import Link from "next/link";
+import { FaHouseChimney } from "react-icons/fa6";
 
 export default function Gallery() {
   return (
-    <div className="font-sans w-full h-[100vh] bg-foreground text-foreground flex flex-col items-center ">
+    <div className="w-full h-[100vh] bg-foreground text-foreground flex flex-col items-center ">
       <div className="px-40 flex absolute justify-between font-serif py-10 text-4xl w-full z-20">
-        <Link href="/">
-          <h1>Ansh Chauhan</h1>
-        </Link>
-        <h1>My Camera Roll Highlights.</h1>
+        <Button href="/" icon={<FaHouseChimney />} alt />
+        <h1 className="font-sans tracking-tighter font-bold">
+          My Camera Roll Highlights.
+        </h1>
       </div>
       <DomeGallery segments={30} />
     </div>
