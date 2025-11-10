3d1882ed50d1e90dd37186eb881a36fa63840200
Ansh Chauhan <anshc2005@gmail.com>
2025-11-10 16:13:11 -0500

    added transition

--- app/designs/page.js
+++ app/designs/page.js
@@ -7,19 +7,28 @@ import Button from "@/components/ui/Button";
 
 export default function Design() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [opacity, setOpacity] = useState(100);
 
   function handleLeftClick() {
-    setCurrentIndex((prev) => {
-      const newIndex = prev === 0 ? data.designs.length - 1 : prev - 1;
-      return newIndex;
-    });
+    setOpacity(0);
+    setTimeout(() => {
+      setOpacity(100);
+      setCurrentIndex((prev) => {
+        const newIndex = prev === 0 ? data.designs.length - 1 : prev - 1;
+        return newIndex;
+      });
+    }, 200);
   }
 
   function handleRightClick() {
-    setCurrentIndex((prev) => {
-      const newIndex = prev === data.designs.length - 1 ? 0 : prev + 1;
-      return newIndex;
-    });
+    setOpacity(0);
+    setTimeout(() => {
+      setOpacity(100);
+      setCurrentIndex((prev) => {
+        const newIndex = prev === data.designs.length - 1 ? 0 : prev + 1;
+        return newIndex;
+      });
+    }, 200);
   }
   const radialBackground = {
     background: `radial-gradient(circle at center, transparent 0%, transparent 60%, ${data.designs[currentIndex].colors.primary}50 100%)`,
@@ -27,7 +36,7 @@ export default function Design() {
 
   return (
     <div
-      className="w-full h-[100vh] bg-background flex items-center justify-center"
+      className={`w-full h-[100vh] bg-background flex items-center justify-center transition duration-200 ease-in-out opacity-${opacity}`}
       style={radialBackground}
     >
       <div className="flex items-center">
