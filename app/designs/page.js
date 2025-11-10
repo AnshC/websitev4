b010b8935a4b404c449d44fb1da5d1f179ade93a
Ansh Chauhan <anshc2005@gmail.com>
2025-11-10 15:46:26 -0500

    initial design page layout

--- app/designs/page.js
+++ app/designs/page.js
@@ -1,6 +1,33 @@
 "use client";
-import styles from "./page.module.css";
+import Design from "@/components/pages/design/Design";
+import data from "@/data/designs.json";
+import { useState } from "react";
 
 export default function Designs() {
-  return <div className="w-full h-[100vh] relative"></div>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function handleLeftClick() {
+    setCurrentIndex((prev) => {
+      const newIndex = prev === 0 ? data.designs.length - 1 : prev - 1;
+      return newIndex;
+    });
+  }
+
+  function handleRightClick() {
+    setCurrentIndex((prev) => {
+      const newIndex = prev === data.designs.length - 1 ? 0 : prev + 1;
+      return newIndex;
+    });
+  }
+
+  return (
+    <div className="w-full h-[100vh] relative">
+      <Design
+        key={data.designs[currentIndex].id}
+        data={data.designs[currentIndex]}
+        handleLeftClick={handleLeftClick}
+        handleRightClick={handleRightClick}
+      />
+    </div>
+  );
 }
