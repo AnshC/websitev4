d0fa273f5c37b5cd898113a985eda0a1820b6363
Ansh Chauhan <anshc2005@gmail.com>
2025-11-10 15:45:03 -0500

    handles onClick functions now

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,15 +1,37 @@
 import Link from "next/link";
-import "./ui.css"
+import "./ui.css";
 
-export default function Button({ children, href, icon, className, alt, iconStyles }) {
+export default function Button({
+  children,
+  href,
+  icon,
+  className,
+  alt,
+  iconStyles,
+  onClick,
+}) {
+  const handleClick = (e) => {
+    if (onClick) {
+      e.preventDefault();
+      onClick(e);
+    }
+  };
+
   return (
-    <div className={`font-sans font-bold text-xl ${className} ${alt ? "text-foreground" : "text-background"}`}>
+    <div
+      className={`font-sans font-bold text-xl ${className} ${alt ? "text-foreground" : "text-background"}`}
+    >
       <Link
         className={`border-3 rounded-full p-3 ${alt ? "border-foreground buttonShadowAlt" : "border-background buttonShadow"} w-max flex items-center`}
         href={href}
+        onClick={handleClick}
       >
         <div>{children}</div>
-        {icon ? <div className={children ? `ml-2 text-2xl` : iconStyles}>{icon}</div> : ""}
+        {icon ? (
+          <div className={children ? `ml-2 text-2xl` : iconStyles}>{icon}</div>
+        ) : (
+          ""
+        )}
       </Link>
     </div>
   );
