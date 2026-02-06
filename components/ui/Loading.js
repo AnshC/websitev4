5e8253245e1ac4c830ba8604fd53e25631eeeeeb
Ansh Chauhan <anshc2005@gmail.com>
2026-02-06 20:00:46 +0100

    packages installed

--- components/ui/Loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading (props) {
-    return (
-        <div className="w-full flex justify-center items-center">
-            <h1 className={`text-3xl ${props.className}`}>Loading...</h1>
-        </div>
-    )
-}
