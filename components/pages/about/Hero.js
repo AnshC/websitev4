5219c8227253c8615ec6d623c1e891401519f377
Ansh Chauhan <anshc2005@gmail.com>
2026-02-17 12:55:09 +0100

    updated layout + img

--- components/pages/about/Hero.js
+++ components/pages/about/Hero.js
@@ -8,16 +8,25 @@ import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 
 export default function Hero() {
-  
   return (
     <div className="bg-background font-serif px-40 py-10 h-full w-full flex flex-col">
       <div className="flex items-center justify-between w-full">
-        <Button href="/" icon={<FaHouseChimney />} alt></Button>
+        <Button href="/" icon={<FaHouseChimney />} alt>
+          Home
+        </Button>
         <h1 className="text-9xl font-sans font-black tracking-tighter px-10 translate-y-5">
           Ansh Chauhan
         </h1>
       </div>
-      <div className="bg-foreground w-full flex-1 rounded-2xl flex overflow-hidden">
+      <div className="bg-foreground w-full flex-1 rounded-4xl flex overflow-hidden">
+        <div className="flex-1 m-10 rounded-4xl relative overflow-hidden">
+          <Image
+            src={"/img/profile.jpeg"}
+            fill={true}
+            style={{ objectFit: "cover" }}
+            alt="Portrait Picture of Ansh"
+          />
+        </div>
         <div className="h-full flex-1 text-background p-10 text-4xl flex flex-col justify-between">
           <div>
             <p className="pb-10">
@@ -33,8 +42,8 @@ export default function Hero() {
               digital experiences.
             </p>
             <p className="pb-10">
-              As a student at the University of Michigan, I'm pursuing a degree
-              in Data Science, alongside a minor in UX Design.
+              I'm an undergraduate student at the University of Michigan,
+              pursuing a degree in Data Science, alongside a minor in UX Design.
             </p>
           </div>
           <div className="flex">
@@ -58,39 +67,6 @@ export default function Hero() {
               href="https://github.com/AnshC"
               icon={<FaGithub />}
             />
-          </div>
-        </div>
-        <div
-          className="h-full flex-1 relative flex items-end justify-center p-10"
-        >
-          <Image
-            className="translate-y-20"
-            src="/img/about.png"
-            alt="About Image"
-            width={400}
-            height={0}
-          />
-        </div>
-        <div className="h-full flex-1 p-10 relative text-background flex items-end justify-end">
-          <div>
-            <h2 className="font-sans font-black tracking-tighter text-7xl text-right">
-              HTML/CSS
-            </h2>
-            <h2 className="font-sans font-black tracking-tighter text-7xl text-right">
-              JavaScript
-            </h2>
-            <h2 className="font-sans font-black tracking-tighter text-7xl text-right">
-              Next.js
-            </h2>
-            <h2 className="font-sans font-black tracking-tighter text-7xl text-right">
-              Python
-            </h2>
-            <h2 className="font-sans font-black tracking-tighter text-7xl text-right">
-              C/C++
-            </h2>
-            <h2 className="font-sans font-black tracking-tighter text-7xl text-right">
-              SwiftUI
-            </h2>
           </div>
         </div>
       </div>
