1b8109ae1606103bc517fceb57d9218bdbca6997
Ansh Chauhan <anshc2005@gmail.com>
2026-02-17 12:54:49 +0100

    removed animation

--- components/pages/home/Hero.js
+++ components/pages/home/Hero.js
@@ -22,32 +22,12 @@ export default function Hero({ designRef }) {
         />
       </div>
       <div className="absolute text-center text-foreground flex flex-col">
-        <SplitText
-          text="Ansh Chauhan"
-          className="text-[200px] font-black tracking-tighter text-shadow-[0_5px_25px_rgb(0_0_0_/_1)] p-1"
-          delay={100}
-          duration={0.5}
-          ease="power3.out"
-          splitType="words"
-          from={{ opacity: 0, y: 40 }}
-          to={{ opacity: 1, y: 0 }}
-          threshold={0.1}
-          rootMargin="0"
-          textAlign="center"
-        />
-        <SplitText
-          text="Web Dev. Design. Coffee."
-          className="font-serif text-6xl p-1 translate-y-[-40px]"
-          delay={100}
-          duration={0.5}
-          ease="power3.out"
-          splitType="words"
-          from={{ opacity: 0, y: 40 }}
-          to={{ opacity: 1, y: 0 }}
-          threshold={0.1}
-          rootMargin="0"
-          textAlign="center"
-        />
+        <h1 className="text-[200px] font-black tracking-tighter p-1">
+          Ansh Chauhan
+        </h1>
+        <p className="font-serif text-6xl p-1 translate-y-[-40px]">
+          Web Dev. Design. Coffee.
+        </p>
       </div>
       <div className="absolute bottom-0 right-0 m-30" onClick={handleClick}>
         <CircularText
