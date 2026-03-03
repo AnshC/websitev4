be4fbe2791e304bb16006c8c36606f409e8749cf
Ansh Chauhan <anshc2005@gmail.com>
2026-03-03 23:51:48 +0100

    added responsiveness

--- components/ui/ShadowText.js
+++ components/ui/ShadowText.js
@@ -1,13 +1,13 @@
 export default function ShadowText({ children, className, alt }) {
   return (
-    <div className={`relative tracking-tighter font-bold text-9xl mb-[25px] w-max h-max ${className}`}>
-      <h2 className="invisible">{children}</h2> 
+    <div
+      className={`relative tracking-tighter font-bold text-7xl md:text-8xl mb-[25px] w-max h-max ${className}`}
+    >
+      <h2 className="invisible">{children}</h2>
+      <h2 className={`absolute top-0`}>{children}</h2>
       <h2
-        className={`absolute top-0`}
+        className={`absolute top-0 translate-x-2 -translate-y-2 ${alt ? "stroke-text-alt" : "stroke-text"} text-foreground`}
       >
-        {children}
-      </h2>
-      <h2 className={`absolute top-0 translate-x-3 -translate-y-3 ${alt ? "stroke-text-alt": "stroke-text"} text-foreground`}>
         {children}
       </h2>
     </div>
