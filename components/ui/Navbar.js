b07330a3b8135102e99ec9b0f1634cfc062ef358
Ansh Chauhan <anshc2005@gmail.com>
2026-03-07 00:22:55 +0100

    fixed page-wide scroll issues

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -4,7 +4,7 @@ import "./ui.css";
 export default function Navbar({ variant }) {
   return (
     <div
-      className={`w-full font-serif fixed top-0 left-0 z-10 transition-colors duration-300
+      className={`pointer-events-none w-full font-serif fixed top-0 left-0 z-10 transition-colors duration-300
           text-2xl px-5 py-10
           sm:text-3xl sm:px-10
           md:text-4xl md:px-30 md:py-10
@@ -16,24 +16,24 @@ export default function Navbar({ variant }) {
     >
       <ul className="flex justify-between">
         <div className="flex">
-          <li className="px-2 sm:px-4">
+          <li className="pointer-events-auto px-2 sm:px-4">
             <Link className="hover-underline-animation left" href="/about">
               About.
             </Link>
           </li>
-          <li className="px-2 sm:px-4">
+          <li className="pointer-events-auto px-2 sm:px-4">
             <Link className="hover-underline-animation left" href="/gallery">
               Gallery.
             </Link>
           </li>
         </div>
         <div className="flex">
-          <li className="px-2 sm:px-4">
+          <li className="pointer-events-auto px-2 sm:px-4">
             <Link className="hover-underline-animation left" href="/designs">
               Designs.
             </Link>
           </li>
-          <li className="px-2 sm:px-4">
+          <li className="pointer-events-auto px-2 sm:px-4">
             <Link className="hover-underline-animation left" href="/projects">
               Projects.
             </Link>
