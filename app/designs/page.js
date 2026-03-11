4c67ecbfba5b2a7455301e6c997d73caa15c8e54
Ansh Chauhan <anshc2005@gmail.com>
2026-03-11 13:53:24 +0100

    responsive design

--- app/designs/page.js
+++ app/designs/page.js
@@ -14,8 +14,8 @@ export default function Design() {
   return (
     <div className="w-full bg-background flex flex-col font-sans">
       <ReactLenis />
-      <div className="px-40 flex absolute justify-between font-serif py-10 text-4xl w-full z-20">
-        <div className="flex items-center justify-center">
+      <div className="flex items-center justify-between font-serif py-10 w-full z-20 px-10 md:px-30">
+        <div className="hidden sm:flex items-center justify-center">
           <Button href="/" icon={<FaHouseChimney />} alt className="mr-5">
             Home
           </Button>
@@ -27,20 +27,35 @@ export default function Design() {
             @ansh.psd
           </Button>
         </div>
-        <h1 className="font-sans tracking-tighter font-light">Ansh Chauhan</h1>
+        <div className="flex sm:hidden">
+          <Button
+            href="/"
+            icon={<FaHouseChimney />}
+            alt
+            className="mr-5"
+          ></Button>
+          <Button
+            href="https://instagram.com/ansh.psd/"
+            icon={<FaInstagram />}
+            alt
+          ></Button>
+        </div>
+        <h1 className="font-sans tracking-tighter font-light text-2xl ml-5">
+          Ansh Chauhan
+        </h1>
       </div>
-      <main className="w-full flex flex-col px-40 py-40">
+      <main className="w-full flex flex-col py-30 px-5 sm:px-10 md:px-30">
         <h1 className="font-serif text-6xl text-center">Design Portfolio.</h1>
         <p className="font-serif text-3xl text-center italic">
           Artwork inspired by cars, music, and personal experiences.
         </p>
       </main>
-      <section className="w-full px-40 flex flex-wrap items-center justify-center z-5 mb-20">
+      <section className="w-full flex flex-wrap items-center justify-center z-5 mb-20 px-5 sm:px-10 md:px-30">
         {items.map((img) => {
           return (
             <Link href={img.img} key={img.id}>
               <div
-                className="w-100 h-100 relative m-5"
+                className="w-75 h-75 md:w-100 md:h-100 relative m-5"
                 onMouseEnter={(e) => {
                   e.currentTarget.style.transition = "box-shadow 0.5s";
                   e.currentTarget.style.boxShadow = `0px 0px 1000px 300px ${img.colors.shadow}`;
@@ -59,7 +74,7 @@ export default function Design() {
                   tiltReverse={true}
                   tiltMaxAngleX={10}
                   tiltMaxAngleY={10}
-                  className="w-100 h-100 cursor-pointer"
+                  className="w-75 h-75 md:w-100 md:h-100 cursor-pointer"
                 >
                   <Image
                     alt={img.title}
